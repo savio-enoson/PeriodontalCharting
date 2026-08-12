bfecc421b7781783d8027eec2d2c5ffb68c96631
Savio Enoson <89328899+savio-enoson@users.noreply.github.com>
2026-08-12 22:13:34 +0700

    remove deprecated test files

--- test_anatomy2.swift
+++
@@ -1,6 +0,0 @@
-import Foundation
-let isRight = (11...18).contains(47) || (41...48).contains(47)
-let isMesial = true
-let siteIndex = isRight ? (isMesial ? 2 : 0) : (isMesial ? 0 : 2)
-print("isRight=\(isRight), isMesial=\(isMesial), siteIndex=\(siteIndex)")
-
